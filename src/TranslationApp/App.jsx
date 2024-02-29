23b9d9793b0dedf8cd5d8987b8790cdc20087e1a
Enayet <enayetflweb@gamil.com>
2024-02-29 10:19:17 +0600

    Language translation app

---
+++ src/TranslationApp/App.jsx
@@ -0,0 +1,12 @@
+import Translate from "./components/Translate";
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="">
+    <Translate/>
+    </div>
+  )
+};
+
+export default App;
